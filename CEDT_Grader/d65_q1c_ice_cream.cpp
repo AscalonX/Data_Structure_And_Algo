c5e42c64fa16e6944922a05ba7b8201417cc3cfa
Tanakit <153923689+AscalonX@users.noreply.github.com>
2025-11-17 18:53:35 +0700

    day7

---
+++ CEDT_Grader/d65_q1c_ice_cream.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+    int n,m,start;
+
+    vector<ll> p(100030);
+
+    
+}
